602f319aa5d138ac8f35088b3a3b4daa47134ebf
abhishek2001kene <abhi2001kene@gmail.com>
2024-12-05 21:45:44 +0530

    connect to mongo

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,20 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import { error } from "console"
+
+dotenv.config()
+
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() =>{
+    console.log("db done")
+})
+.catch((error) =>{
+    console.log(error)
+})
+
+
 const app = express()
 
 
