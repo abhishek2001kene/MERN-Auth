fb5769cfe322dca6ad31051e61e2c0c119fd9ab0
abhishek2001kene <abhi2001kene@gmail.com>
2024-12-05 19:58:47 +0530

    Add routes

--- Frontend/mern/src/App.jsx
+++ Frontend/mern/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from './components/header'
+
+
 
 function App() {
   return (
-    <div>App</div>
+    <>
+<Header/>
+<Outlet/>
+    </>
   )
 }
 
--- Frontend/mern/src/main.jsx
+++ Frontend/mern/src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {RouterProvider, createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom" 
+import Login from './components/Login.jsx'
+import SignUp from './components/SignUp.jsx'
+import Home from './components/Home.jsx'
+
+const router = createBrowserRouter([
+ {
+  path:"/",
+  element:<App/>,
+  children:[
+    {
+      path:"/",
+      element:<Home/>
+    },
+    {
+      path:"/login",
+      element:<Login/>
+    },
+    
+    {
+      path:"/signup",
+      element:<SignUp/>
+    },
+
+  ]
+
+ }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ <React.StrictMode>
+  <RouterProvider router={router}/>
+ </React.StrictMode>
 )
