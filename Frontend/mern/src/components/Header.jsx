8a15feb787609cb24084938fcd011fe03ecec17c
abhishek2001kene <abhi2001kene@gmail.com>
2024-12-05 20:13:52 +0530

    add Nav bar and links

--- Frontend/mern/src/components/Header.jsx
+++ Frontend/mern/src/components/Header.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='bg-slate-800 flex justify-between py-3 px-32 items-center '>
+       <Link>
+       <div className='text-white'>
+            Auth App
+        </div>
+       </Link>
+        <ul className='flex justify-between p-4 gap-10 text-white items-center'>
+            <Link to='/'>Home</Link>
+            <Link to='login'>Login</Link>
+            <Link to="signup">Sign Up</Link>
+        </ul>
+    </div>
   )
 }
 
